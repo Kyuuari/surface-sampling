6c5008025e1ce77facc1bcde6c81c95f8305e47a
Chester Cari <chester_cari@outlook.com>
2023-04-18 11:52:20 -0400

    added small sample scene

--- src/components/Experience.tsx
+++ src/components/Experience.tsx
@@ -1,21 +1,47 @@
-import { Grid, OrbitControls } from "@react-three/drei";
+import { Grid, OrbitControls, Point, Points, Sampler } from "@react-three/drei";
 import { useControls } from "leva";
 import { Perf } from "r3f-perf";
-import React from "react";
+import React, { useRef } from "react";
+import { InstancedMesh, Mesh } from "three";
 
 type Props = {};
 
 const Experience = (props: Props) => {
   const { scale } = useControls({ scale: -2 });
+  const instRef = useRef<InstancedMesh>(null!);
+  const geoRef = useRef<Mesh>(null!);
+
   return (
     <>
       <Perf position={"top-left"} />
       <OrbitControls />
       <Grid cellColor="white" args={[10, 10]} />
-      <mesh scale={scale}>
+
+      <Sampler count={500}>
+        <mesh>
+          <boxGeometry args={[5, 5, 5]} />
+          <meshNormalMaterial wireframe />
+        </mesh>
+
+        {/* <Points
+          limit={1000} // Optional: max amount of items (for calculating buffer size)
+          range={1000} // Optional: draw-range
+        >
+          <pointsMaterial vertexColors />
+          <Point color="red" />
+        </Points> */}
+
+        <instancedMesh args={[null!, null!, 1_000]}>
+          <sphereGeometry args={[0.1, 32, 32]} />
+          <meshNormalMaterial />
+        </instancedMesh>
+      </Sampler>
+
+      {/* <mesh scale={scale} ref={geoRef}>
         <boxGeometry args={[1, 1, 1]} />
-        <meshBasicMaterial />
-      </mesh>
+        <meshBasicMaterial wireframe />
+      </mesh> */}
+      {/* <Sampler mesh={mesh} instances={instances}></Sampler> */}
     </>
   );
 };
